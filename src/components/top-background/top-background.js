408415b7ddd63fdbb1a97a541e3d017253b531fc
Marina Kravchenko <marishka-k87@yandex.ru>
2024-11-11 21:01:20 +0300

    fix background

--- src/components/top-background/top-background.js
+++ src/components/top-background/top-background.js
@@ -2,11 +2,12 @@ import styles from "./top-background.module.css";
 
 export default function TopBackground() {
   return (
-    <div className={styles.background}>
-      <div className={styles.back_1}></div>
-      <div className={styles.back_2}></div>
-      <div className={styles.back_3}></div>
-    </div>
+    <div className = {styles.content}>
+      <div className = {styles.back_color}>
+          <div className = {styles.back_img}></div>
+          <div className = {styles.back_mask}></div>
+          <div className = {styles.front_img}></div>
+      </div>
+  </div>
   );
 }
-
