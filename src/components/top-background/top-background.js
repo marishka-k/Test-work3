20d370f8617e00f45fff1a0fe9071ba24a9df0f9
Marina Kravchenko <marishka-k87@yandex.ru>
2026-05-15 20:56:22 +0300

    fix background

--- src/components/top-background/top-background.js
+++ src/components/top-background/top-background.js
@@ -1,13 +1,6 @@
 import styles from "./top-background.module.css";
+import backgroundImg from '../../images/main_image.png'; 
 
 export default function TopBackground() {
-  return (
-    <div className = {styles.content}>
-      <div className = {styles.back_color}>
-          <div className = {styles.back_img}></div>
-          <div className = {styles.back_mask}></div>
-          <div className = {styles.front_img}></div>
-      </div>
-  </div>
-  );
+  return <div className={styles.background} aria-hidden="true" />;
 }
